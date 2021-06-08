fd1fd6e5986b78a47449335eeb02a6da0cbd6a38
The Real Tech Lady <bukolaarowosebi@gmail.com>
2021-06-08 10:33:11 +0100

    Installed axios, react-chat-engine and antdesign icons. Implemented the react chat engine with props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
+import { ChatEngine } from "react-chat-engine";
+import "./App.css";
 
-function App(){
-    return(
-        <div>
-            New App
-        </div>
-    )
-}
+const App = () => {
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="83329391-ab94-4482-bfd5-1e5e4d0f3008"
+      userName="ladybee"
+      userSecret="ladybee"
+    />
+  );
+};
 
 export default App;
